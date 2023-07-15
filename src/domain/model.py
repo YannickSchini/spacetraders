f449c538cf0124869e6cc12f092dbb6d6e2327c7
Yannick Schini <yannick.schini@backmarket.com>
2023-07-15 19:23:29 +0200

    feat(waypoint): Added traits and abstract repo
    This will allow filtering waypoints based on specific traits, for
    example when looking for MARKETPLACE or SHIPYARD waypoints.

--- src/domain/model.py
+++ src/domain/model.py
@@ -1,14 +1,22 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
 
 
 @dataclass
 class Waypoint():
     waypoint: str
+    traits: List[str] = field(default_factory=list)
 
     def __repr__(self) -> str:
-        return f"Waypoint {self.waypoint}"
+        return f"Waypoint {self.waypoint} with traits {self.traits}"
+
+class WaypointRepository(ABC):
+    @abstractmethod
+    def get_waypoints_in_system(self,
+                                target_system: str,
+                                agent_token: str) -> List[Waypoint]:
+        raise NotImplementedError
 
 @dataclass
 class Delivery():
