bc33f2906f4dad9ea782cfba658127eb2d0f83ab
Yannick Schini <yannick.schini@backmarket.com>
2023-08-15 15:33:38 +0200

    refactor(test): Rework the existing test
    Two actions are taken here:
    - The test was testing two different things independently. I've now
      split it in two, so I could name each one more aptly, so it's name
      describes it's intent more clearly
    - I've modified the tests to use the new create_agent_repo() Factory
      method (this should have been done 2 commits ago, but was missed)

--- tests/unit/test_purchase_ship.py
+++ tests/unit/test_purchase_ship.py
@@ -1,5 +1,3 @@
-
-from src.domain.agent import Agent
 from src.domain.waypoint import Waypoint
 from src.orchestration.purchase_ship import (
     find_local_shipyard,
@@ -9,38 +7,40 @@ from src.repositories.agent import InMemoryAgentRepository
 from src.repositories.waypoint import InMemoryWaypoinyRepository
 
 
-def test_find_a_shipyard() -> None:
-    agent_1 = Agent("DUMMY_TOKEN", Waypoint("A1-BB2-WP001"))
-    system_with_shipyard = {"A1-BB2": [
+def test_find_an_existing_shipyard_in_the_local_system() -> None:
+    system_with_shipyard = {"X1-YY2": [
         Waypoint("coord1", ["NOT COOL", "TOO HOT", "I'M DYING HERE"]),
         Waypoint("coord2", ["COLD", "SO COLD", "IT'S LIKE HOTH ALL OVER AGAIN"]),
         Waypoint("coord3", ["MARKETPLACE", "SHIPYARD"]),
         ]
     }
-    agent_2 = Agent("DUMMY_TOKEN", Waypoint("A2-BB3-WP001"))
-    system_without_shipyard = {"A2-BB3": [
-        Waypoint("single_planet", ["NOT A MARKETPLACE", "NOT A SHIPYARD"]),
-        ]
-    }
-
-    agent_repo_1 = InMemoryAgentRepository(agent_1)
-    agent_repo_2 = InMemoryAgentRepository(agent_2)
-    waypoint_repo = InMemoryWaypoinyRepository(
-            system_without_shipyard | system_with_shipyard)
+    agent_repo = InMemoryAgentRepository.create_agent_repo()
+    waypoint_repo = InMemoryWaypoinyRepository(system_with_shipyard)
 
     assert find_local_shipyard(
-                agent_repo_1,
+                agent_repo,
                 waypoint_repo
             ) == \
             Waypoint("coord3",
                      ["MARKETPLACE", "SHIPYARD"]
             )
-    assert find_local_shipyard(agent_repo_2, waypoint_repo) is None
 
-def test_list_all_ship_types() -> None:
-    agent_repo = InMemoryAgentRepository(Agent("DUMMY_TOKEN", Waypoint("A1-BB2-CCCC5")))
+
+def test_find_a_shipyard_returns_none_when_no_shipyard_exists_locally() -> None:
+    system_without_shipyard = {"X1-YY2": [
+        Waypoint("single_planet", ["NOT A MARKETPLACE", "NOT A SHIPYARD"]),
+        ]
+    }
+
+    agent_repo = InMemoryAgentRepository.create_agent_repo()
+    waypoint_repo = InMemoryWaypoinyRepository(system_without_shipyard)
+
+    assert find_local_shipyard(agent_repo, waypoint_repo) is None
+
+def test_list_all_ship_types_from_a_local_shipyard() -> None:
+    agent_repo = InMemoryAgentRepository.create_agent_repo()
     shipyard = Waypoint("coord3", ["MARKETPLACE", "SHIPYARD"])
-    system_with_shipyard = {"A1-BB2": [
+    system_with_shipyard = {"X1-YY2": [
         Waypoint("coord1", ["NOT COOL", "TOO HOT", "I'M DYING HERE"]),
         Waypoint("coord2", ["COLD", "SO COLD", "IT'S LIKE HOTH ALL OVER AGAIN"]),
         shipyard,
