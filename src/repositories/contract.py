e8a5216ec90b2b37226b1eb55d223d0c8b574cad
Yannick Schini <yannick.schini@backmarket.com>
2023-08-15 15:26:02 +0200

    refactor(contract): Change in-memory contract repo
    This is done to try and decouple our test from our domain so we're freer
    to refactor our domain later on (see Chapter 5 in Cosmic Python).
    
    We add a new Factory method to the Contract repository module that
    produces an InMemoryContractRepository. We also add a new method to the
    InMemoryContractRepository: it allows us to create a contract using only
    base data types.
    
    This means that if/when we'll change the implementation of our
    Contract class, we will only have to change the Factory method instead of
    going through all our tests and manually fixing them one by one.

--- src/repositories/contract.py
+++ src/repositories/contract.py
@@ -47,8 +47,8 @@ class HttpContractRepository(ContractRepository):
         )
 
 class InMemoryContractRepository(ContractRepository):
-    def __init__(self, contracts: List[Contract]):
-        self.contracts = contracts
+    def __init__(self) -> None:
+        self.contracts: List[Contract] = []
 
     def get_contracts(self, agent_token: str) -> List[Contract]:
         logger.info("Getting the contracts")
@@ -60,3 +60,30 @@ class InMemoryContractRepository(ContractRepository):
             return None
         else:
             raise ValueError(f"Contract ID {contract_id} does not exist.")
+
+    def append_contract_to_repo(
+        self,
+        contract_id: str = "DUMMY_CONTRACT_1",
+        merchandise: str = "ROCK",
+        destination: str = "THERE",
+        units_required: int = 100,
+        units_fulfilled: int = 0
+    ) -> None:
+        self.contracts.append(
+            Contract(
+                contract_id=contract_id,
+                delivery_details=[
+                    Delivery(
+                        merchandise=merchandise,
+                        destination=Waypoint(destination),
+                        units_required=units_required,
+                        units_fulfilled=units_fulfilled,
+                    )
+                ]
+            )
+        )
+
+def create_in_memory_contract_repo() -> InMemoryContractRepository:
+    repo = InMemoryContractRepository()
+    repo.append_contract_to_repo()
+    return repo
--- tests/unit/test_contracts.py
+++ tests/unit/test_contracts.py
@@ -1,51 +1,39 @@
-from typing import List
-
-from src.domain.agent import Contract, Delivery
-from src.domain.waypoint import Waypoint
 from src.repositories.agent import InMemoryAgentRepository
-from src.repositories.contract import InMemoryContractRepository
+from src.repositories.contract import create_in_memory_contract_repo
 
-contracts = [
-    Contract(
-        contract_id="DUMMY_CONTRACT_1",
-        delivery_details=[
-            Delivery(
-                merchandise="ROCK",
-                destination=Waypoint("THERE"),
-                units_required=100,
-                units_fulfilled=0,
-            ),
-        ]
-    ),
-    Contract(
+
+def test_get_contracts_returns_contracts() -> None:
+    agent_repo = InMemoryAgentRepository.create_agent_repo()
+    contracts_repo = create_in_memory_contract_repo()
+    contracts_repo.append_contract_to_repo(
         contract_id="DUMMY_CONTRACT_2",
-        delivery_details=[
-            Delivery(
-                merchandise="ROCK",
-                destination=Waypoint("THERE"),
-                units_required=10000,
-                units_fulfilled=0,
-            ),
-            Delivery(
-                merchandise="GOLD",
-                destination=Waypoint("ALL_THE_WAY_OVER_THERE"),
-                units_required=100,
-                units_fulfilled=5,
-            ),
-        ]
-    ),
-]
-
-def test_get_contracts_returns_contracts(contracts: List[Contract] = contracts) -> None:
-    agent_repo = InMemoryAgentRepository.create_agent_repo()
-    contracts_repo = InMemoryContractRepository(contracts)
+        merchandise="GOLD",
+        destination="HERE",
+        units_required=105,
+        units_fulfilled=5,
+    )
     agent = agent_repo.get_agent()
+    first_contract_from_repo = agent.get_contracts(contracts_repo)[0]
+    second_contract_from_repo = agent.get_contracts(contracts_repo)[1]
 
-    assert contracts == agent.get_contracts(contracts_repo)
+    assert len(agent.get_contracts(contracts_repo)) == 2
 
-def test_accept_contract(contracts: List[Contract] = contracts) -> None:
+    assert first_contract_from_repo.contract_id == "DUMMY_CONTRACT_1"
+    assert first_contract_from_repo.delivery_details[0].merchandise == "ROCK"
+    assert first_contract_from_repo.delivery_details[0].destination.waypoint == "THERE"
+    assert first_contract_from_repo.delivery_details[0].units_required == 100
+    assert first_contract_from_repo.delivery_details[0].units_fulfilled == 0
+
+    assert second_contract_from_repo.contract_id == "DUMMY_CONTRACT_2"
+    assert second_contract_from_repo.delivery_details[0].merchandise == "GOLD"
+    assert second_contract_from_repo.delivery_details[0].destination.waypoint == "HERE"
+    assert second_contract_from_repo.delivery_details[0].units_required == 105
+    assert second_contract_from_repo.delivery_details[0].units_fulfilled == 5
+
+
+def test_accept_contract() -> None:
     agent_repo = InMemoryAgentRepository.create_agent_repo()
-    contracts_repo = InMemoryContractRepository(contracts)
+    contracts_repo = create_in_memory_contract_repo()
     agent = agent_repo.get_agent()
     contracts = agent.get_contracts(contracts_repo)
 
